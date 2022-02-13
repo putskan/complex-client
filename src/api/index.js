8edb6b15f97d0213672c429e87dd38865f444846
putskan <ben.lahav101@gmail.com>
2022-02-13 22:30:05 +0200

    fix: SERVER_URL env var

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-axios.defaults.baseURL = process.env.VUE_APP_SERVER_URL;
+axios.defaults.baseURL = process.env.VUE_APP_SERVER_URL || "http://127.0.0.1:8000/";
 
 export default {
     getQuestionList(page, size) {
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
-// process.env.VUE_APP_SERVER_URL = "http://127.0.0.1:8000/"
-
 module.exports = {
   transpileDependencies: ["vuetify"],
 };
